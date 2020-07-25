3674ef99e6824f284402b81f20ab1e907220d0e8
Harut <harut_intern@franqbox.com>
2020-07-26 00:54:16 +0400

    rerendered to hooks

--- src/components/SeacrhBar.js
+++ src/components/SeacrhBar.js
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = {
-      term:''
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit(term);
   };
 
-  onFormSubmit = e => {
-    e.preventDefault()
-    this.props.onFormSubmit(this.state.term)
-  }
-
-  render() {
-    return (
-      <div className='search-bar ui segment'>
-        <form onSubmit={this.onFormSubmit} className='ui form'>
-          <div className='field'>
-            <label>Video search</label>
-            <input value={this.state.term} type='text' onChange={e => {this.setState({term:e.target.value})}} />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video search</label>
+          <input
+            value={term}
+            type="text"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 export default SearchBar;
